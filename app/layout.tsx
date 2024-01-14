d0781fac4b714d18a1d628f6ccbc769215f8ce4c
Piyush Aryaprakash <piyusharyaprakash12345@gmail.com>
2024-01-14 08:15:38 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
 
-import { Providers } from './providers'
-
-const inter = Poppins({ subsets: ['latin'],
-weight: ["100", "200", "300", "400", "500"], })
+import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500"],
+});
 
 export const metadata: Metadata = {
-  title: 'PrimeFlix',
-  description: 'Watch movies and series for free',
-}
+  title: "PrimeFlix",
+  description: "Watch movies and series for free",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <Providers>
-        {children}
+          {children}
+          <Analytics />
         </Providers>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
