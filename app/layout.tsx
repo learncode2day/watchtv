be0fa47811d4df0008c1a28238001248efb27055
theo <townhelpemail@gmail.com>
2024-03-21 11:28:34 -0400

    try to remove ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,15 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <head>
-      <meta name="monetag" content="6609151acc6ca0fa38eae33c4a90fe53"/>
-      </head>
       <body className={inter.className}>
         <Providers>
           {children}
           <ClarityScript />
         </Providers>
-      <script src="https://alwingulla.com/88/tag.min.js" data-zone="46726" async data-cfasync="false"></script>
       </body>
     </html>
   );
